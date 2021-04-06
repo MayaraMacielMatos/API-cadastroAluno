d4be7fc6cf532d3bf550ea2b070e026552fb70ac
admin <admin@Mayaras-iMac.local>
2021-04-06 01:20:10 -0300

    models

---
+++ api_aluno/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class CadastroAluno(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=120)
+    cpf = models.IntegerField()
+    number = models.IntegerField()
+    email = models.CharField(max_length=120)
+    create_at = models.DateTimeField(auto_now_add=True)

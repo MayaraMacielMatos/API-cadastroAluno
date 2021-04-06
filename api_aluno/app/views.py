74d5cdd77e930b0948178dc1814eb6bd9fe232ae
admin <admin@Mayaras-iMac.local>
2021-04-06 17:21:21 -0300

    Generics

--- api_aluno/app/views.py
+++ api_aluno/app/views.py
@@ -1,47 +1,14 @@
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.exceptions import NotFound
+from rest_framework import generics
 
 from app.models import StudentRegistration
 from app.serializers import StudentRegistrationSerializer
 
 
-class StudentListAndCreate(APIView):
-    def get(self, request):
-        registration = StudentRegistration.objects.all()
-        serializer = StudentRegistrationSerializer(registration, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = StudentRegistrationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class StudentListAndCreate(generics.ListCreateAPIView):
+    queryset = StudentRegistration.objects.all()
+    serializer_class = StudentRegistrationSerializer
 
 
-class StudentChangeAndDelete(APIView):
-    def get_object(self, pk):
-        try:
-            return StudentRegistration.objects.get(pk=pk)
-        except StudentRegistration.DoesNotExist:
-            raise NotFound()
-
-    def get(self, request, pk):
-        student = self.get_object(pk)
-        serializer = StudentRegistrationSerializer(student)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        student = self.get_object(pk)
-        serializer = StudentRegistrationSerializer(student, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        student = self.get_object(pk)
-        student.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class StudentChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = StudentRegistration.objects.all()
+    serializer_class = StudentRegistrationSerializer
